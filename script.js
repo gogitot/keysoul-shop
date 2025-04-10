364849fb63a6a4ae5ece5beb38936e3ac9ed0dee
gogitot <rgstrategytime@gmail.com>
2025-04-10 16:27:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,21 @@
 (function () {
-    emailjs.init("cl3jFq_E1Gew5d3j");
+  emailjs.init("cl3jFq_E1Gew5d3j");
 
-    document.getElementById('contact-form').addEventListener('submit', function (event) {
-        event.preventDefault();
+  document.getElementById('contact-form').addEventListener('submit', function (event) {
+    event.preventDefault();
 
-        const popup = document.getElementById('popup');
-        const popupError = document.getElementById('popup-error');
+    const popup = document.getElementById('popup');
+    const popupError = document.getElementById('popup-error');
 
-        emailjs.send('service_fbm7d8s', 'template_jzaceu8', {
-            name: this.name.value,
-            email: this.email.value,
-            message: this.message.value
-        })
-            .then(() => {
-                this.reset();
-                popup.classList.add('show');
-                setTimeout(() => popup.classList.remove('show'), 4000);
-            })
-            .catch(() => {
-                popupError.classList.add('show');
-                setTimeout(() => popupError.classList.remove('show'), 4000);
-            });
-    });
+    emailjs.sendForm('service_fbm7d8s', 'template_jzaceu8', this)
+      .then(() => {
+        this.reset();
+        popup.classList.add('show');
+        setTimeout(() => popup.classList.remove('show'), 4000);
+      })
+      .catch(() => {
+        popupError.classList.add('show');
+        setTimeout(() => popupError.classList.remove('show'), 4000);
+      });
+  });
 })();
