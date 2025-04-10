e1b14b805e95c3161061951ac8bedb427c652e28
gogitot <rgstrategytime@gmail.com>
2025-04-10 16:52:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 (function () {
   emailjs.init("public_cl3jFq_E1Gew5d3j");
 
-  document.getElementById('contact-form').addEventListener('submit', function (event) {
+  const form = document.getElementById('contact-form');
+form.addEventListener('submit', function (event) {
     event.preventDefault();
 
     const popup = document.getElementById('popup');
     const popupError = document.getElementById('popup-error');
 
-    emailjs.sendForm('service_fbm7d8s', 'template_jzaceu8', this)
+    emailjs.sendForm('service_fbm7d8s', 'template_jzaceu8', form)
       .then(() => {
         this.reset();
         popup.classList.add('show');
